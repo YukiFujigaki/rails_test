f1eb2cf551e1c330657b2625c63cbe64f0d1708d
Yuki Fujigaki <yuki.fj5@gmail.com>
2021-05-03 00:28:44 +0900

    戻す

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   subject { User.new(name: '名前', email: 'メール') }
-  it { expect(subject.name).to eq('名前test') }
+  it { expect(subject.name).to eq('名前') }
   it { expect(subject.email).to eq('メール') }
 end
